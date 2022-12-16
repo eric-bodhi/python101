74d212dbe829248f6c3fa07bd6d59a6d148ce782
Eric Floyd <erixefb@gmail.com>
2022-12-15 21:56:53 -0500

    changed some things

--- sudoku/solver.py
+++ sudoku/solver.py
@@ -3,36 +3,62 @@ import logging as log
 from validSudoku import isValidSudoku
 from generator import generate
 from random import sample
+    #Helper Functions
+
 
 log.basicConfig(format="%(levelname)s:%(message)s", level=log.DEBUG)
+class Sudoku:
+    def __init__(self, board):
+        self.board = board
 
-#Helper Functions
-def make_playable(board):
-    for i in sample(range(81), 5):
-        board[i//9][i%9] = 0
-    
-    return board
+    def make_playable(self):
+        for i in sample(range(81), 80):
+            self.board[i//9][i%9] = 0
 
-def cords(row, col):
-    return [(row-1)//3, (col-1)//3]
+    def validMove(self, val, row, col):
+        if self.board[row][col] != 0:
+            return False
 
-def possibleRow(row, col, num, board):
-    row = board[row]
-    return num not in row and row[col] == 0
+        if val in self.board[row]:
+            return False
+            
+        elif val in [i[col] for i in self.board]:
+            return False
+            
+        for m in range((row//3) * 3, (row//3) * 3 + 3):
+            for n in range((col//3) * 3, (col//3)*3 + 3):
+                if (row,col) != (m,n) and self.board[m][n] == val:
+                    return False
+        return True
+        
+    def findEmptyCell(self):
+        for i in range(len(self.board)):
+            for j in range(len(self.board)):
+                if self.board[i][j] == 0:
+                    return (i, j)
 
-def possibleCol(row, col, num, board):
-    line = [board[i][col] for i in range(9)]
-    print(line)
-    return num not in line and line[row] == 0
+    def solve(self):
+        emptyCell = self.findEmptyCell()
 
-def possibleSquare(row, col, num, board):
-    r, c = cords(row+1, col+1)[0] * 3, cords(row+1, col+1)[1] * 3
-    square = sum([board[r+i][c:c+3] for i in range(3)], [])
-    log.debug()
-    return num not in square and board[row][col] == 0
+        if not emptyCell:
+            return board
+        
+        row, col = emptyCell
+        for num in range(1, 10):
+            if self.validMove(num, row, col):
+                self.board[row][col] = num
+
+                if self.solve():
+                    return True
+            self.board[row][col] = 0
+        
+        return False
 
 
-solved_board = generate()
-board = make_playable(solved_board)
-prev_board = board[:]
-print("hi")
+board = Sudoku(generate())
+board.make_playable()
+
+log.debug(board.board)
+
+board.solve()
+log.debug(board.board)
